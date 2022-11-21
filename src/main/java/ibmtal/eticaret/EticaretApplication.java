beee51898a85ea5e86b9c91fc9251ac155c30ac6
OnurKonuk174 <onurkonuk174@gmail.com>
2022-11-21 14:37:47 +0300

    Initial commit

---
+++ src/main/java/ibmtal/eticaret/EticaretApplication.java
@@ -0,0 +1,14 @@
+package ibmtal.eticaret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableSwagger2
+public class EticaretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EticaretApplication.class, args);
+	}
+
+}
